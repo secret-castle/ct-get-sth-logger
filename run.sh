f2dee20ae1dc26e6e54e2e849763ea766434d538
Milon Maze <42852009+milonmaze@users.noreply.github.com>
2018-08-31 00:28:33 +0000

    initial commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prerequisites: 
+#
+# 1. Generate the list:
+# $ curl -s 'https://cs.chromium.org/codesearch/f/chromium/src/components/certificate_transparency/data/log_list.json' | jq -r '.logs[]|.url'
+#
+# 2. After the list is generated, manually review it, and comment out servers that are dead.
+
+# 
+# Then run the following query
+#
+
+echo "fetching get-sth from ct log list."
+for api in $(cat ctlogs.txt | grep -v '^#')
+do 
+  api=${api%/}
+  curl -s -m 10 --retry 3 -w '\n' https://$api/ct/v1/get-sth >> ct-get-sth-log/$api/timestamp.txt &
+done
+wait 
+
+echo "done."
