eaf87ea7886cbaf37c806aef0b6935f20510494a
Milon Maze <42852009+milonmaze@users.noreply.github.com>
2018-08-31 05:30:31 +0000

    add auto push

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPO=github.com/secret-castle/ct-get-sth-log
+DIR=ct-get-sth-log
+
+git_config() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+git_commit() {
+  git add --all
+  git commit --message "[ci-skip] Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+git_push() {
+  git remote add ci https://${GH_TOKEN}@${REPO}.git > /dev/null 2>&1
+  git push --quiet ci master
+}
+
+pushd ${DIR}
+git_config
+git_commit
+git_push
+popd
